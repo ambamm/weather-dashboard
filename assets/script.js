a66173f1ce4ad71f397c8d54c585ed1efff711de
Amber <amberpitts@gmail.com>
2022-12-13 19:58:52 -0700

    setting the repo up

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+//variables
+//functions
+//event listeners 
+
+
+//api key: e3f5192af11f96c2ca0a8644dabb537a
+
+
+//search button for user to select their city
+
+
+//  city name executes a (current condition) to get request weather
+
+
+//Parsing responses to receive current conditions
+    
+
+//Using saved city name, display a 5 day forecast
+        //Local storage?
+
+//Parse response to display forecast for next 5 days underneath current conditions?
+
+//initPage?
